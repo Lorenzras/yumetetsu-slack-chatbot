10aadeb3deafc777aaeebc32209cb1c3c2d081ff
Lorenz Ras <lenzras@gmail.com>
2022-03-14 12:55:10 +0900

    fixed display name

--- src/controllers/slackEvents/interaction/modal/openHankyoContentsModal.ts
+++ src/controllers/slackEvents/interaction/modal/openHankyoContentsModal.ts
@@ -3,13 +3,12 @@ import {ActionButton,
 } from '../../../../types/slack';
 
 import {KintoneAppRecord,
-    KintoneHankyoTaiouRecord,
 } from '../../../../types/kintone';
 import {getRecord} from '../../../kintone/kintone';
 
 import sendModal from '../../api/sendModal';
 import hankyoContents from '../../../../view/slack/modals/hankyoContents';
-import {getDisplayName} from '../../api/getUser';
+// import {getDisplayName} from '../../api/getUser';
 
 const openHankyoContentsModal = async (
     actionButton : ActionButton, payload: InteractionPayload,
@@ -20,7 +19,7 @@ const openHankyoContentsModal = async (
 
     const kintoneRecord = (
         await getRecord(kintoneRecordId)
-    )?.record as unknown as KintoneHankyoTaiouRecord;
+    )?.record as unknown as hankyo.SavedFields;
 
     const emailBody = kintoneRecord
         ?.main.value?.toString() ?? 'Error. Contact Lenz. ';
@@ -28,12 +27,14 @@ const openHankyoContentsModal = async (
         ?.taiouJiko.value.join(', ') ?? 'Error. Contact Lenz. ';
     const biko = kintoneRecord
         ?.biko.value ?? 'Error. Contact Lenz. ';
+    const name = kintoneRecord
+        .slackDisplayName?.value || kintoneRecord.slackUser.value;
 
     sendModal(
         payload.trigger_id,
         hankyoContents(
             {
-                name: (await getDisplayName(payload.user.id)),
+                name,
                 emailBody,
                 taiouJiko,
                 biko,
---
+++ src/types/hankyoFields.d.ts
@@ -0,0 +1,28 @@
+declare namespace hankyo {
+    interface Fields {
+        slackThread: kintone.fieldTypes.MultiLineText;
+        mail_to: kintone.fieldTypes.SingleLineText;
+        biko: kintone.fieldTypes.MultiLineText;
+        slackChannel: kintone.fieldTypes.SingleLineText;
+        main: kintone.fieldTypes.MultiLineText;
+        mail_from: kintone.fieldTypes.SingleLineText;
+        title: kintone.fieldTypes.SingleLineText;
+        文字列__1行__1: kintone.fieldTypes.SingleLineText;
+        slackUser: kintone.fieldTypes.SingleLineText;
+        slack_link: kintone.fieldTypes.Link;
+        slackTS: kintone.fieldTypes.SingleLineText;
+        ルックアップ_0: kintone.fieldTypes.SingleLineText;
+        slackDisplayName: kintone.fieldTypes.SingleLineText;
+
+        taiouJiko: kintone.fieldTypes.CheckBox;
+    }
+    interface SavedFields extends Fields {
+        $id: kintone.fieldTypes.Id;
+        $revision: kintone.fieldTypes.Revision;
+        更新者: kintone.fieldTypes.Modifier;
+        作成者: kintone.fieldTypes.Creator;
+        レコード番号: kintone.fieldTypes.RecordNumber;
+        更新日時: kintone.fieldTypes.UpdatedTime;
+        作成日時: kintone.fieldTypes.CreatedTime;
+    }
+}
