9b0865799e8e4bbc8c19a008b085607dd33ea5be
Lorenz Ras <lenzras@gmail.com>
2022-03-19 14:58:31 +0900

    remove dist

--- dist/controllers/kintone/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateKintoneLink = void 0;
-const generateKintoneLink = ({ appId, recordId }, isEdit = false) => {
-    return `https://rdmuhwtt6gx7.cybozu.com/k/${appId}/show#record=${recordId}&mode=${isEdit ? 'edit' : 'show'}`;
-};
-exports.generateKintoneLink = generateKintoneLink;
-//# sourceMappingURL=helpers.js.map
--- dist/middlewares/slack.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const events_api_1 = require("@slack/events-api");
-const constants_1 = require("../UTIL/constants");
-const slackEvents = (0, events_api_1.createEventAdapter)(constants_1.SLACK_SIGNING_SECRET);
-exports.default = slackEvents;
-//# sourceMappingURL=slack.js.map
