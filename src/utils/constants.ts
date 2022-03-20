5b3b2e8fc8280e9142763a33b8eaaf96200d363a
Lorenz Ras <lenzras@gmail.com>
2022-03-20 13:05:44 +0900

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,21 +1,8 @@
-
-/* export const SLACK_BOT_TOKEN = process
-  .env.SLACK_BOT_TOKEN;
-
-export const KINTONE_API_TOKEN_TOYOKAWA = process
-  .env.KINTONE_API_TOKEN_TOYOKAWA;
-
-export const KINTONE_API_TOKEN_TOYOHASHI = process
-  .env.KINTONE_API_TOKEN_TOYOHASHI;
-
-export const KINTONE_API_TOKEN = process
-  .env.KINTONE_API_TOKEN;
- */
-/* export const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET; */
+/*
 
 export const KINTONE_HANKYO_TOYOKAWA_APP_ID = '155';
 export const KINTONE_HANKYO_TOYOHASHI_APP_ID = '167';
 export const KINTONE_HANKYO_APP_ID = '187';
-export const KINTONE_DOMAIN = process.env.KINTONE_DOMAIN;
+export const KINTONE_DOMAIN = process.env.KINTONE_DOMAIN; */
 
 
